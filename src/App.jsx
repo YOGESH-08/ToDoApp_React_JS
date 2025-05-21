e8418065ea16de92d4bce116d07112c3165a6218
Yogesh Kumar <yogeshkumar.n1234@gmail.com>
2025-05-21 16:36:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import React,{useEffect, useState} from "react";
+import "./App.css"
+
+
+
+
+function App(){
+
+  document.addEventListener('keydown',(event)=>{
+    if(event.key==="Enter"){
+      handleAddClick();
+    }
+  });
+
+
+
+const date = new Date();
+
+  const [startAdd,setStartAdd] = useState(false);
+  const [newTask,setNewTask] = useState("");
+  const [task,setTask] = useState([]);
+  const [anyThingThere,setAnyThingThere] = useState(false);
+
+
+function handleAddClick(){
+  setStartAdd(true);
+}
+
+
+function handleWindow(){
+  if(!anyThingThere){
+    return <>
+    <div className="transparent">
+      NO Task ToDO </div></>
+  }
+}
+
+function clearAll(){
+  if(anyThingThere){
+    setNewTask("");
+    setTask([]);
+    setAnyThingThere(false);
+    setStartAdd(false);
+  }
+}
+
+
+function handleAddTaskClick() {
+  if (newTask.trim() !== "") {
+    const taskObj = {
+      text: newTask.trim(),
+      date: new Date().toLocaleString(), 
+    };
+    setTask((prev) => [...prev, taskObj]);
+    setNewTask("");
+    setStartAdd(false);
+    setAnyThingThere("true");
+  }
+}
+
+function handleInputChange(event){
+  setNewTask(event.target.value);
+}
+  function taskWindow(){
+    const handleKeyDownInput = (event) => {
+    if (event.key === "Enter") {
+      handleAddTaskClick();
+    }
+  };
+    return(
+    <div className="enterTask">
+      <label htmlFor="item">New Task &nbsp;</label>
+      <input type="text" value={newTask} onChange={handleInputChange}
+      onKeyDown={handleKeyDownInput}/>
+      <button className="addtask" onClick={handleAddTaskClick}>Add Task</button>
+  </div>);
+    
+  }
+
+  useEffect(()=>{
+  if (startAdd){
+    const inputField = document.querySelector("input[type='text']");
+  if(inputField){
+    inputField.focus();     
+  }
+}
+},[startAdd]);
+
+ 
+
+
+  return <>
+  <div className="container">
+    <div className="header">
+      <h1 className="gradient-text">To-Do</h1>
+      <button onClick={handleAddClick}>Add + </button>
+    </div>
+      {startAdd ? taskWindow():handleWindow()}
+  </div>
+    <div className="todocard">
+  {task.map((todo, index) => (
+    <div className="Card" key={index} onClick ={()=>{
+      const filtered = task.filter((deltask,i)=>i!==index);
+      setTask(filtered);
+         if (filtered.length === 0) {
+        setAnyThingThere(false);
+      }
+    }}>
+      <p>{todo.text}</p><br />
+      <p className="date">{todo.date}</p>
+    </div>
+  ))}
+
+  
+</div>
+<div className="footer">
+    <button onClick={clearAll}>Clear All</button>
+  </div>
+
+    </>;
+}
+
+export default App;
