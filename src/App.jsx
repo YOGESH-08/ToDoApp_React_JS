7561746787c132f40305f62b2695fa07a53640b2
Yogesh Kumar <yogeshkumar.n1234@gmail.com>
2025-05-22 14:39:24 +0530

    add edit button only

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,9 @@ function handleInputChange(event){
       setTask(updatedTasks);
     }}
   >
-    <p>{todo.text}</p><br />
+    <p>{todo.text}</p><div onClick = {(e)=>{
+      e.stopPropagation();
+    }}className="edit"><svg className="edit" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg></div><br />
     <p className="date">{todo.date}</p>
     <div
     onClick = {(e)=>{
@@ -130,10 +132,11 @@ function handleInputChange(event){
         handleWindow();
       }
     }}>
-      <IconButton 
+      <div className="dustbin"><IconButton 
       aria-label="delete" disabled color="primary">
         <DeleteIcon  />
       </IconButton>
+      </div>
   </div>
   </div>
 ))}
